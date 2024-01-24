53302b9c3811f5a3e15e31566a06357c65cfe879
sergio-coti <cotiaulajava@outlook.com>
2024-01-24 08:54:13 -0300

    fix: correção Infra.Messages

---
+++ PedidosApi.Infra.Messages/Events/PedidoCriadoEvent.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using PedidosApi.Domain.Interfaces.Events;
+using PedidosApi.Domain.Models;
+using PedidosApi.Infra.Messages.Settings;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace PedidosApi.Infra.Messages.Events
+{
+    public class PedidoCriadoEvent : IPedidoCriadoEvent
+    {
+        private readonly RabbitMQSettings? _rabbitMQSettings;
+
+        public PedidoCriadoEvent(RabbitMQSettings? rabbitMQSettings)
+        {
+            _rabbitMQSettings = rabbitMQSettings;
+        }
+
+        /// <summary>
+        /// Gravar os dados do pagamento na fila (Message Broker)
+        /// </summary>
+        public async Task RealizarPagamento(PedidoCriadoModel model)
+        {
+            var connectionFactory = new ConnectionFactory { Uri = new Uri(_rabbitMQSettings.Url) };
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    #region Criando a fila
+
+                    channel.QueueDeclare(
+                        queue: _rabbitMQSettings.Queue,
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    channel.BasicPublish(
+                        exchange: string.Empty,
+                        routingKey: _rabbitMQSettings.Queue,
+                        basicProperties: null,
+                        body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(model))
+                        );
+
+                    #endregion
+                }
+            }
+        }
+    }
+}
---
+++ PedidosApi.Infra.Messages/Extensions/MessagesExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using PedidosApi.Domain.Interfaces.Events;
+using PedidosApi.Infra.Messages.Events;
+using PedidosApi.Infra.Messages.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApi.Infra.Messages.Extensions
+{
+    public static class MessagesExtension
+    {
+        public static IServiceCollection AddMessages(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Capturando as configurações do /appsettings.json
+            var rabbitMQSettings = new RabbitMQSettings();
+            new ConfigureFromConfigurationOptions<RabbitMQSettings>
+                (configuration.GetSection("RabbitMQSettings")).Configure(rabbitMQSettings);
+
+            services.AddSingleton(rabbitMQSettings);
+            services.AddTransient<IPedidoCriadoEvent, PedidoCriadoEvent>();
+
+            return services;
+        }
+    }
+}
